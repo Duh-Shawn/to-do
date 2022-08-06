4859662ef9effb360bdfda2ab41207803bd8a224
De'Shawn <deshawn.nathaniel.maynard@gmail.com>
2022-08-06 14:15:56 -0400

    Add local storage

--- src/controller.js
+++ src/controller.js
@@ -15,23 +15,30 @@ class Controller {
         e.preventDefault();
         const name = document.getElementById('name').value;
         this.projectList.push(new Project(name));
+        localStorage.setItem('projectList', JSON.stringify(this.projectList));
         this.ui.displayProjectList(this.projectList);
     }
 
     setSelectedProject = (e) => {
         const projectIndex = e.target.parentNode.dataset.indexNumber;
         this.selectedProject = this.projectList[projectIndex];
-        this.ui.displayProjectTasks(this.selectedProject);
+        console.log(this.projectList);
         console.log(this.selectedProject);
+        console.log(JSON.parse(localStorage.getItem('projectList')));
+        localStorage.setItem('projectList', JSON.stringify(this.projectList));
+        this.ui.displayProjectTasks(this.selectedProject);
     }
 
     removeProjectFromList = (index) => {
+        if (this.projectList[index] === this.selectedProject) location.reload(); 
         this.projectList.splice(index, 1);
+        localStorage.setItem('projectList', JSON.stringify(this.projectList));
     }
 
     deleteProject = (e) => {
         const projectIndex = e.target.parentNode.dataset.indexNumber;
         this.removeProjectFromList(projectIndex);
+        localStorage.setItem('projectList', JSON.stringify(this.projectList));
         this.ui.displayProjectList(this.projectList);
     }
 
@@ -44,12 +51,16 @@ class Controller {
         const priority = document.getElementById('priority').value; 
         const task = new Task(title, description, dueDate, priority);
         this.selectedProject.addTask(task);
+        console.log(this.projectList);
+        console.log(this.selectedProject);
+        localStorage.setItem('projectList', JSON.stringify(this.projectList));
         this.ui.displayProjectTasks(this.selectedProject);
     }
 
     deleteTask = (e) => {
         const taskIndex = e.target.parentNode.dataset.indexNumber;
         this.selectedProject.removeTask(taskIndex);
+        localStorage.setItem('projectList', JSON.stringify(this.projectList));
         this.ui.displayProjectTasks(this.selectedProject);
     }
 
--- src/index.js
+++ src/index.js
@@ -4,30 +4,45 @@ import { Task } from "./task";
 import { Controller } from "./controller";
 import { Events } from "./events";
 
-const defaultProject = new Project("Default");
-const projectList = [];
-projectList.push(defaultProject);
-let selectedProject = defaultProject;
+const main = () => {
 
-const controller = new Controller(projectList, selectedProject);
-const events = new Events(controller);
-events.init();
+    //first time app is ever run
+    if (JSON.parse(localStorage.getItem('projectList')) === null){
+        let defaultProject = new Project("Default");
+        let projectList = [];
+        projectList.push(defaultProject);
+        localStorage.setItem('projectList', JSON.stringify(projectList));
+    }
+    else{
+        const localProjectList = JSON.parse(localStorage.getItem('projectList'));
+        let defaultProject;
+        let projectList = [];
+        if (localProjectList.length === 0){ //local storage is empty
+            defaultProject = new Project("Default");
+            projectList = [];
+            projectList.push(defaultProject);
+        }
+        else{
+            localProjectList.forEach(jsonProject => {
+                const jsonTaskList = jsonProject.taskList;
+                const project = new Project(jsonProject.name);
+                jsonTaskList.forEach(jsonTask => {
+                    const task = new Task(jsonTask.title, jsonTask.description, jsonTask.dueDate, jsonTask.priority);
+                    project.addTask(task);
+                });
+                projectList.push(project);
+            });
+            defaultProject = projectList[0];
+        }
+
+        const controller = new Controller(projectList, defaultProject);
+        const events = new Events(controller);
+        events.init();
+        const ui = new UI(controller);
+        ui.displayProjectTasks(defaultProject);
+        ui.displayProjectList(projectList); 
+    }
+}
 
 
-/* 
-const ui = new UI(controller);
-
-
-const task1 = new Task("title", "description", "12/08/1996", "low");
-const task2 = new Task("title", "description", "12/08/1996", "low");
-const task3 = new Task("title", "description", "12/08/1996", "low");
-const task4 = new Task("title", "description", "12/08/1996", "low");
-selectedProject.addTask(task1);
-selectedProject.addTask(task2);
-selectedProject.addTask(task3);
-selectedProject.addTask(task4);
-
-
-ui.displayProjectTasks(selectedProject);
-ui.displayProjectList(projectList);
-    */
+main();
