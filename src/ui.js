6babfd9af003f608f79653ca3923d717e86593bc
De'Shawn <deshawn.nathaniel.maynard@gmail.com>
2022-08-10 12:23:50 -0400

    Provide sanitization of user inputs being rendered in HTML

--- src/ui.js
+++ src/ui.js
@@ -9,33 +9,39 @@ class UI {
         this.events = new Events(controller);
     }
 
-    static closeTaskInfo = () => {
-        document.querySelector('.task-info-popup').style.display = 'none';
-        document.querySelector('.bg-modal').style.display = 'none';
-    }
+
+   //Sanitize and encode HTML from user inputted values
+    sanitizeHTML = (input) => {
+        const temp = document.createElement('div');
+        temp.textContent = input;
+        return temp.innerHTML;
+    };
+
+
 
     displayTaskInfo = (task, project) => {
         document.querySelector('.bg-modal').style.display = 'flex';
         const taskInfoPopup = document.querySelector('.task-info-popup');
         taskInfoPopup.innerHTML = `<div class="close-info-popup">+</div><div class="task-info">
+
         
     <div class="task-info-title">
-        <span>Title: </span> <span>${task.title}</span>
+        <span>Title: </span> <span>${this.sanitizeHTML(task.title)}</span>
     </div>
     <div class="task-info-project">
-        <span>Project: </span> <span>${project.name}</span>
+        <span>Project: </span> <span>${this.sanitizeHTML(project.name)}</span>
     </div>
     <div class="task-info-completion">
-        <span>Completed: </span> <span>${task.isCompleted}</span>
+        <span>Completed: </span> <span>${this.sanitizeHTML(task.isCompleted)}</span>
     </div>
     <div class="task-info-priority">
-        <span>Priority: </span> <span>${task.priority}</span>
+        <span>Priority: </span> <span>${this.sanitizeHTML(task.priority)}</span>
     </div>
     <div class="task-info-date">
-        <span>Due Date: </span> <span>${task.dueDate}</span>
+        <span>Due Date: </span> <span>${this.sanitizeHTML(task.dueDate)}</span>
     </div>
     <div class="task-info-details">
-        <span>Description: </span> <span class="task-info-description">${task.description}</span>
+        <span>Description: </span> <span class="task-info-description">${this.sanitizeHTML(task.description)}</span>
     </div></div>`;
         document.querySelector('.bg-modal').appendChild(taskInfoPopup);
         taskInfoPopup.style.display = 'block';
@@ -48,7 +54,7 @@ class UI {
             const projectDiv = document.createElement('div');
             projectDiv.classList = "project";
             projectDiv.dataset.indexNumber = i;
-            projectDiv.innerHTML=`<p class="project-name">${projectList[i].name}</p><div class="remove-project">+</div>`
+            projectDiv.innerHTML=`<p class="project-name">${this.sanitizeHTML(projectList[i].name)}</p><div class="remove-project">+</div>`
             
             this.events.initProject(projectDiv); //attach events to project
             document.querySelector('.projects-container').appendChild(projectDiv);
@@ -77,7 +83,7 @@ class UI {
             const taskDiv = document.createElement('div');
             taskDiv.classList = "task";
             taskDiv.dataset.indexNumber = i;
-            taskDiv.innerHTML=`<div class="task-left"><input class="task-completed-checkbox" type="checkbox"><p class="task-title">${task.title}</p></div><div class="task-right"><p class="task-due">${task.dueDate}</p><img class="task-info-icon"><img class="task-edit-icon"><div class="remove-task">+</div></div>`;
+            taskDiv.innerHTML=`<div class="task-left"><input class="task-completed-checkbox" type="checkbox"><p class="task-title">${this.sanitizeHTML(task.title)}</p></div><div class="task-right"><p class="task-due">${this.sanitizeHTML(task.dueDate)}</p><img class="task-info-icon"><img class="task-edit-icon"><div class="remove-task">+</div></div>`;
             // const editLogo = taskDiv.querySelector('.edit-icon');
             taskDiv.querySelector('.task-info-icon').src = info;
             taskDiv.querySelector('.task-edit-icon').src = edit;
@@ -89,6 +95,8 @@ class UI {
             document.querySelector('.project-data').appendChild(taskDiv);
         }
     }
+
+    //popups and forms
 
     static openTaskForm = () => {
         document.querySelector('.bg-modal').style.display = 'flex';
@@ -114,6 +122,12 @@ class UI {
         document.querySelector('.bg-modal').style.display = 'flex';
         document.querySelector('.new-project-popup').style.display = 'flex';
     }
+
+    static closeTaskInfo = () => {
+        document.querySelector('.task-info-popup').style.display = 'none';
+        document.querySelector('.bg-modal').style.display = 'none';
+    }
+
 }
 
 
